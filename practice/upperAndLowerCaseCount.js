4284f2a8d8caacf1416b289d1597e0b3b8118351
vanishap <vanishareddyp@gmail.com>
2023-03-09 06:34:07 -0800

    using ASCII code

--- practice/upperAndLowerCaseCount.js
+++ practice/upperAndLowerCaseCount.js
@@ -1,3 +1,4 @@
+//Count the lower case and upper case letters
 
 function lowerAndUpperCount(str){
     const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -16,3 +17,21 @@ function lowerAndUpperCount(str){
    
 }
 lowerAndUpperCount('hellO My NamE iS VaniSHa');
+
+
+//ASCII Uppercase A=65,..... Z=90
+// lowercase a=97,....z=122
+function caseCount(string){
+ let lowerCount = 0;
+ let upperCount =0;
+    for(let i=0;i<string.length;i++){
+       if (string[i].charCodeAt() >=65 && string[i].charCodeAt() <= 90){
+        upperCount++
+       }else{
+        lowerCount++
+       }
+    }
+    console.log(`upper count,  ${upperCount}`);
+    console.log(`lowerCount, ${lowerCount}`)
+}
+caseCount('hellO My NamE iS VaniSHa');
