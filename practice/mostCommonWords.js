7668bcf1c0105dca36d0e8617a2119773d7f670d
vanishap <vanishareddyp@gmail.com>
2023-06-26 10:06:08 -0700

    practice

--- practice/mostCommonWords.js
+++ practice/mostCommonWords.js
@@ -1,6 +1,6 @@
 const mostCommonWords = (text)=>{
 
-    const words = text.toLowerCase().replace(/[",./';:?]/g, "").split(' ').sort();
+    const words = text.toLowerCase().replace(/[",./';:?]/g, "").split(' ');
     const commonWords = words.reduce((acc,ini)=>{
         if(acc[ini]){
             acc[ini]++;
@@ -8,9 +8,11 @@ const mostCommonWords = (text)=>{
             acc[ini] =1;
         }
         return acc;
-    },[]);
-    
-    return commonWords;
-   
+    },{});
+    for(const [key,value] of Object.entries(commonWords)){
+        // let ans = value.sort((a,b)=>b-a);
+        return Object.values(commonWords).sort((a,b)=>b-a);
+    }
+  
 };
 console.log(mostCommonWords('It was the best of times, it was the worst of times.'))
